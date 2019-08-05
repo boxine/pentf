a1a761712a6982ce921717572682159c26ac3310
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2019-08-05 12:41:30 +0200

    net_utils: + setupTLSClientAuth helper function
    This helper function can be used to quickly initialize client-side TLS authentication for an HTTPS request.
    In particular, it will make the curl output usable.

--- net_utils.js
+++ net_utils.js
@@ -4,6 +4,8 @@ const node_fetch = require('node-fetch');
 
 const {makeCurlCommand} = require('./curl_command');
 const output = require('./output');
+const {readFile} = require('./utils');
+
 
 async function fetch(config, url, options) {
     if (!options) options = {};
@@ -35,6 +37,24 @@ async function fetch(config, url, options) {
     return await node_fetch(url, options);
 }
 
+async function setupTLSClientAuth(fetchOptions, keyFilename, certFilename, rejectUnauthorized=false) {
+    const agentOptions = {
+        rejectUnauthorized,
+        keepAlive: true,
+    };
+    agentOptions.key = await readFile(keyFilename, 'binary');
+    agentOptions.cert = await readFile(certFilename, 'binary');
+    fetchOptions.agent = new https.Agent(agentOptions);
+    if (!fetchOptions.curl_extra_options) {
+        fetchOptions.curl_extra_options = [];
+    }
+    fetchOptions.curl_extra_options.push(... [
+        '--key', keyFilename,
+        '--cert', certFilename,
+    ]);
+}
+
 module.exports = {
     fetch,
+    setupTLSClientAuth,
 };

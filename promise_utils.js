83ed654e416b1a3e11525376583ebf7d4660f8eb
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2019-08-07 17:46:15 +0200

    Add promise_utils library
    These are not strictly necessary to use pintf, but make it easier to write good test cases.

---
+++ promise_utils.js
@@ -0,0 +1,31 @@
+// Avoid UnhandledPromiseRejectionWarning if a promise fails before we use it.
+// Use like this:
+// const email_promise = catchLater(getMail(...));
+// await ...
+// const email = await email_promise;
+function catchLater(promise) {
+    promise.catch(() => undefined);
+    return promise;
+}
+
+// await and emit a custom message if it fails
+async function customErrorMessage(promise, error_message) {
+    try {
+        return await promise;
+    } catch (e) {
+        e.message += ' (' + error_message + ')';
+        if (! e.stack.includes(error_message)) {
+            // Some exception classes generate the stack automatically
+            const newline_index = e.stack.indexOf('\n');
+            if (newline_index >= 0) {
+                e.stack = e.stack.slice(0, newline_index) + ' (' + error_message + ')' + e.stack.slice(newline_index);
+            }
+        }
+        throw e;
+    }
+}
+
+module.exports = {
+    catchLater,
+    customErrorMessage,
+};

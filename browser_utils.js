boxine/pentf

commit b9f2ef2ee063373ff0d89f6554f6ec3ca7bf6d71

browser_utils: Use initial browser page
When we launch a new browser, it comes with a page already. Use that page instead of creating a new one, because it's faster and gets rid of the useless blank page distracting humans.

browser_utils.js
@@ -86,7 +86,7 @@ async function newPage(config, chrome_args=[]) {
         });
     }
 
-    return browser.newPage();
+    return (await browser.pages())[0];
 }
 
 async function closePage(page) {

commit 57881a635e1b81d6ea5e2c52759e1c7d08debe73

browser_utils: Fix --devtools-preserve
The `--devtools-preserve` option configures the Chrome devtools to preserve network requests, which is quite helpful if a test runs through many pages before we want or even could reconfigure the devtools to log all requests.

We broke this accidentally with b9f2ef2ee063373ff0d89f6554f6ec3ca7bf6d71 , because now the first page has devtools already, and we use it straightaway.

In addition, chrome now useses the `devtools://` pseudo-protocol name instead of the former `chrome-devtools://`, so look at that as well.

browser_utils.js
@@ -58,10 +58,11 @@ async function newPage(config, chrome_args=[]) {
         };
     }
     const browser = await puppeteer.launch(params);
+    const page = (await browser.pages())[0];
 
     if (config.devtools_preserve) {
-        browser.on('targetcreated', async target => {
-            if (! /^chrome-devtools:\/\//.test(await target.url())) {
+        const configureDevtools = async (target) => {
+            if (! /^(?:chrome-)?devtools:\/\//.test(await target.url())) {
                 return;
             }
 
@@ -83,10 +84,14 @@ async function newPage(config, chrome_args=[]) {
                 })).result.value;
             }, 'could not toggle preserve options in devtools', 10000, 100);
             await session.detach();
-        });
+        };
+
+        browser.on('targetcreated', configureDevtools);
+        const targets = await browser.targets();
+        await Promise.all(targets.map(t => configureDevtools(t)));
     }
 
-    return (await browser.pages())[0];
+    return page;
 }
 
 async function closePage(page) {
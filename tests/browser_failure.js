b1360cc655db835a3ab953f0e55ed67b04680a8c
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2019-10-31 01:53:03 +0100

    browser_failure: fix indentation
    By accident I wrote that file with tabs rather than spaces.

--- tests/browser_failure.js
+++ tests/browser_failure.js
@@ -1,22 +1,22 @@
 const {closePage, newPage} = require('../browser_utils');
 
 async function run(config) {
-	const closed = await newPage(config);
-	await closed.setContent('already closed');
-	await closePage(closed);
+    const closed = await newPage(config);
+    await closed.setContent('already closed');
+    await closePage(closed);
 
-	const page0 = await newPage(config);
-	await page0.setContent('This is the first page');
+    const page0 = await newPage(config);
+    await page0.setContent('This is the first page');
 
-	const foreground = await newPage(config);
-	await foreground.setContent('<style>*{background:#f88;}</style>This is the second page');
+    const foreground = await newPage(config);
+    await foreground.setContent('<style>*{background:#f88;}</style>This is the second page');
 
-	throw new Error('Test failed (this will cause screenshots)');
+    throw new Error('Test failed (this will cause screenshots)');
 }
 
 module.exports = {
-	description: 'Reproduces a browser failure. This is not a selftest; more of a demo.',
-	resources: [],
-	run,
-	skip: () => !process.env.PINTF_DEMO,
+    description: 'Reproduces a browser failure. This is not a selftest; more of a demo.',
+    resources: [],
+    run,
+    skip: () => !process.env.PINTF_DEMO,
 };

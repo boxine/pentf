bdf2ae47f0bc686e245f2d068ebaa87485c2e6fe
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2020-03-31 21:39:56 +0200

    Include tags in version determination
    When the current commit is tagged, display the tags as well.
    
    Also use the system's EOL instead of harcoding `\n`.

--- version.js
+++ version.js
@@ -1,5 +1,6 @@
 const child_process = require('child_process');
 const {promisify} = require('util');
+const {EOL} = require('os');
 
 async function _cmd(cmd, args, options) {
     return (await (promisify(child_process.execFile)(cmd, args, options))).stdout.trim();
@@ -7,17 +8,22 @@ async function _cmd(cmd, args, options) {
 
 async function testsVersion(config) {
     try {
+        const tagsOutput = await _cmd(
+            'git', ['tag', '--points-at', 'HEAD'], {cwd: config._testsDir});
+        const tags = tagsOutput.split(EOL).filter(line => line);
+        const tagsRepr = (tags.length > 0) ? tags.join('/') + '/' : '';
+
         const gitVersion = await _cmd(
             'git', ['show', '--pretty=format:%h (%ai)', '--no-patch', 'HEAD'],
             {cwd: config._testsDir});
-        const changesStr = await _cmd('git', ['status', '--porcelain'], {cwd: config._testsDir});
+        const changesOutput = await _cmd('git', ['status', '--porcelain'], {cwd: config._testsDir});
         const changedFiles = (
-            changesStr.split('\n')
+            changesOutput.split(EOL)
                 .filter(line => line)
                 .map(line => line.trim().split(/\s+/, 2)[1]));
         const suffix = (changedFiles.length > 0) ? `+changes(${changedFiles.join(' ')})` : '';
 
-        return gitVersion + suffix;
+        return tagsRepr + gitVersion + suffix;
     } catch(e) {
         return 'unknown';
     }

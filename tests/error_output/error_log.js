a4ee7111b2ddc2e71912bc515d028532b1e6cfdd
Marvin Hagemeister <hello@marvinh.dev>
2020-07-27 17:24:22 +0200

    Re-print errored test before summary

---
+++ tests/error_output/error_log.js
@@ -0,0 +1,8 @@
+async function run() {
+    throw new Error('fail');
+}
+
+module.exports = {
+    description: 'Fail',
+    run,
+};
---
+++ tests/selftest_error_summary.js
@@ -0,0 +1,45 @@
+const assert = require('assert').strict;
+const path = require('path');
+const child_process = require('child_process');
+
+async function run() {
+    {
+        // Run in subprocess so that handle exhaustion does not affect this process
+        const sub_run = path.join(__dirname, 'error_output', 'run');
+        const {stdout} = await new Promise((resolve, reject) => {
+            child_process.execFile(
+                sub_run,
+                ['--exit-zero', '--no-screenshots', '--no-color'],
+                (err, stdout, stderr) => {
+                    if (err) reject(err);
+                    else resolve({stdout, stderr});
+                }
+            );
+        });
+
+        assert.equal((stdout.match(/test\scase\serror_log/g) || []).length, 1, 'do not repeat error output at the end');
+    }
+
+    {
+        // Run in subprocess so that handle exhaustion does not affect this process
+        const sub_run = path.join(__dirname, 'error_output', 'run');
+        const {stdout} = await new Promise((resolve, reject) => {
+            child_process.execFile(
+                sub_run,
+                ['--exit-zero', '--no-screenshots', '--no-color', '-v'],
+                (err, stdout, stderr) => {
+                    if (err) reject(err);
+                    else resolve({stdout, stderr});
+                }
+            );
+        });
+
+        assert.equal((stdout.match(/test\scase\serror_log/g) || []).length, 2, 'print error output again at the end');
+    }
+}
+
+module.exports = {
+    description: 'Test error stack trace collection logged after runner completed',
+    resources: [],
+    run,
+};

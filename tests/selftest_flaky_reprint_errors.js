190f9f5a414855c31f2904b817864dfc56fa4654
Marvin Hagemeister <hello@marvinh.dev>
2020-08-18 12:11:11 +0200

    Don't re-print errors if the test is flaky

---
+++ tests/selftest_flaky_reprint_errors.js
@@ -0,0 +1,25 @@
+
+const assert = require('assert').strict;
+const path = require('path');
+const child_process = require('child_process');
+
+async function run() {
+    const sub_run = path.join(__dirname, 'flaky_tests', 'run');
+    const {stdout} = await new Promise((resolve, reject) => {
+        child_process.execFile(
+            sub_run,
+            ['--exit-zero', '--no-colors', '--no-screenshots', '--repeat-flaky', '3', '--ci', '-f', 'flaky'],
+            (err, stdout, stderr) => {
+                if (err) reject(err);
+                else resolve({stdout, stderr});
+            }
+        );
+    });
+
+    assert.equal((stdout.match(/Error: fail/g) || []).length, 2);
+}
+
+module.exports = {
+    description: 'Don\'t include flaky tasks when re-printing errors',
+    run,
+};

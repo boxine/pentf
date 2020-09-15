22f62eef14b0c457d64b88fafaa3c8aa4a6deacb
Marvin Hagemeister <hello@marvinh.dev>
2020-09-16 00:01:13 +0200

    WIP

--- src/server/public/browser-runner.js
+++ src/server/public/browser-runner.js
@@ -25,4 +25,50 @@
     socket.addEventListener('message', function (event) {
         console.log('Message from server ', event.data);
     });
+
+    var testCases = new Set();
+    var ctx = [];
+    function addTestCase(desc, fn, type) {
+        testCases.add({
+            id: ctx.join('>') + desc,
+            desc,
+            fn,
+            type, // only | skip
+        });
+    }
+    var test = function(desc, fn) {
+        addTestCase(desc, fn, 'default');
+    };
+    test.only = function(desc, fn) {
+        addTestCase(desc, fn, 'only');
+    };
+    test.skip = function(desc, fn) {
+        addTestCase(desc, fn, 'skip');
+    };
+    var describe = function(desc, fn) {
+        ctx.push(desc);
+        fn();
+    };
+
+    window.test = test;
+    window.it = test;
+    window.describe = describe;
+    window.suite = describe;
+
+    // Tests will be collected after first tick
+    Promise.resolve().then(function() {
+        var json = [];
+        testCases.forEach(function (testCase) {
+            json.push({
+                id: testCase.id,
+                desc: testCase.desc,
+                type: testCase.type,
+            });
+        });
+
+        socket.send(JSON.stringify({
+            type: 'add-tests',
+            tests: json,
+        }));
+    });
 })();

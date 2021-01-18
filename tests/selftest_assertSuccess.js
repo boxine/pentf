f837b78f3b2ada1ef2a9d420b8746701ccb7e448
Marvin Hagemeister <hello@marvinh.dev>
2021-01-18 12:06:38 +0100

    Fix retryUntil failing when original selector is not present

--- tests/selftest_assertSuccess.js
+++ tests/selftest_assertSuccess.js
@@ -32,17 +32,44 @@ async function testFn(fn) {
 
 async function run(config) {
     const page = await newPage(config);
-    await page.setContent(`
+    const content = `
         <div>
             <button onclick="pentfClick('first')">first</button>
             <div data-testid="invisible" style="display:none;" onclick="pentfClick('invisible')">invisible</div>
         </div>
-    `);
+    `;
+    await page.setContent(content);
 
     await testFn((options) => clickSelector(page, 'button', options));
     await testFn((options) => clickXPath(page, '//button', options));
     await testFn((options) => clickText(page, 'first', options));
     await testFn((options) => clickNestedText(page, 'first', options));
+
+    // Should succeed when original elements are removed, but the
+    // callback function passes.
+    let visible = true;
+    const retryUntil = async () => {
+        if (visible) {
+            await page.setContent('<div></div>');
+            visible = false;
+            return false;
+        }
+        return true;
+    };
+
+    await clickSelector(page, 'button', {retryUntil});
+
+    await page.setContent(content);
+    visible = true;
+    await clickXPath(page, '//button', {retryUntil});
+
+    await page.setContent(content);
+    visible = true;
+    await clickText(page, 'first', {retryUntil});
+
+    await page.setContent(content);
+    visible = true;
+    await clickNestedText(page, 'first', {retryUntil});
 
     await closePage(page);
 }

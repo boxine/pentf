1251965476254b595a26839a018c33eb690342ed
Marvin Hagemeister <hello@marvinh.dev>
2021-02-25 09:36:59 +0100

    Fix incorrect error message with multiple matches

--- tests/selftest_waitForSelector.js
+++ tests/selftest_waitForSelector.js
@@ -1,15 +1,16 @@
 const assert = require('assert').strict;
 
-const {closePage, newPage, waitForSelector} = require('../src/browser_utils');
+const {newPage, waitForSelector} = require('../src/browser_utils');
 
 async function run(config) {
     const page = await newPage(config);
     await page.setContent(`
         <div style="display:none;" id="a"></div>
-        <div style="visibility: hidden;" id="b">This is b</div>
+        <div style="visibility: hidden;" id="b" class="hidden">This is b</div>
         <div id="c" class="foo">this is c</div>
         <div style="display:none;" id="d"></div>
         <div id="e" class="foo">this is e</div>
+        <div id="f" class="hidden">this is f</div>
     `);
 
     await assert.rejects(waitForSelector(page, '#a', {timeout: 100, message: 'display is none'}), {
@@ -27,14 +28,13 @@ async function run(config) {
     const c = await waitForSelector(page, '#c', {timeout: 1000});
     assert.strictEqual(await page.evaluate(c => c.innerText, c), 'this is c');
 
-    const c2 = await waitForSelector(page, '.foo', {timeout: 1000});
-    assert.strictEqual(await page.evaluate(c2 => c2.innerText, c2), 'this is c');
+    const foo = await waitForSelector(page, '.foo', {timeout: 1000});
+    assert.strictEqual(await page.evaluate(foo => foo.innerText, foo), 'this is c');
 
-    await closePage(page);
+    await assert.rejects(waitForSelector(page, '.hidden', {timeout: 1000}), {message: /There are 1 more/});
 }
 
 module.exports = {
     description: 'Testing browser_utils.waitForSelector.',
-    resources: [],
     run,
 };

2dfeae689023737334278b23eb30cf3c2a7e21f7
Marvin Hagemeister <hello@marvinh.dev>
2021-03-21 09:55:50 +0100

    Fix binary node not found in child_process
    This happens because child processes are not executed in the same shell
    the main process was started in

--- tests/selftest_ts_esbuild.js
+++ tests/selftest_ts_esbuild.js
@@ -6,7 +6,7 @@ async function run() {
     const script = path.join(__dirname, '..', 'bin', 'cli.js');
     const {stderr} = await new Promise((resolve, reject) => {
         child_process.execFile(
-            'node',
+            process.execPath,
             [script, '--exit-zero', '--no-screenshots', '--no-pdf'],
             (err, stdout, stderr) => {
                 if (err) reject(err);

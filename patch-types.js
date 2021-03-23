5f111927f82fb8bc3edd042f8d51806c3b7cd7d3
Marvin Hagemeister <hello@marvinh.dev>
2021-03-23 02:32:02 +0100

    Fix types for package exports not found.
    Newer TypeScript versions have trouble resolving package entries via an
    ambient declaration file. They seem to always traverse the directory
    directly instead and disregard what's defined in package.json, except for
    the main entry.
    
    The only solution that works is to store types in the root folder again
    
    See: https://github.com/microsoft/TypeScript/issues/33079

--- patch-types.js
+++ patch-types.js
@@ -1,41 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-function addModule(name, content) {
-    const type = content
-        .replace(/declare function/g, 'function')
-        .split('\n').map(x => '    ' + x).join('\n');
+// TypeScript doesn't understand package exports, so we need
+// to pretend that every entry is a top-level one. See:
+// https://github.com/microsoft/TypeScript/issues/33079
+const typesDir = path.join(__dirname, 'dist', 'types');
+const files = fs.readdirSync(typesDir)
+    .map(file => path.join(typesDir, file));
 
-    return `\ndeclare module "${name}" {\n${type}\n}\n`;
+for (const file of files) {
+    fs.copyFileSync(file, path.join(__dirname, path.basename(file)));
 }
-
-const entries = Object.keys(require('./package.json').exports)
-    .filter(x => x !== './package.json' && x !== './');
-
-let main = '';
-
-const moduleReg = /(?:from\s['"]([A-Za-z0-9_\-./]+)['"]|import\(['"]([A-Za-z0-9_\-./]+)['"]\))/g;
-
-for (const entry of entries) {
-    const name = entry === '.' ? 'index' : entry.slice(2);
-    const type = fs.readFileSync(path.join(__dirname, 'dist', 'types', name + '.d.ts'), 'utf-8');
-    const mod = name === 'index' ? 'pentf' : 'pentf/' + name;
-
-    main += addModule(mod, type)
-        // Rewrite module imports inside pentf to reference each other.
-        // TS doesn't allow relative imports in an ambient TS file.
-        .replace(moduleReg, (match, staticImport, dynamicImport) => {
-            let source = staticImport || dynamicImport;
-            if (!source.startsWith('./') || source === './internal') {
-                return match;
-            }
-
-            source = `pentf/${source.slice(2)}`;
-
-            return staticImport ? `from "${source}"` : `import("${source}")`;
-        });
-}
-
-const target = path.join(__dirname, 'dist', 'types', '__index.d.ts');
-fs.writeFileSync(target, main);
-
